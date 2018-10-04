50616002b1e91ebb4bfb728466755f52a7414aff
Abdulmalek Al-Gahmi <aalgahmi@EH313-00-14436.local>
2018-10-04 10:20:05 -0600

    After thu class

--- app/controllers/posts.controller.js
+++ app/controllers/posts.controller.js
@@ -9,26 +9,32 @@ router.get('/', function(req, res, next) {
   })
 });
 
-/* GET /posts/:id -> view.ejs */
-router.get('/:id', function(req, res, next) {
-  Post.find().exec((err, posts) => {
-      res.render('index', { title: 'My Blog', posts });
-  })
-});
-
 
 /* GET  /posts/new -> new.ejs */
 router.get('/new', function(req, res, next) {
-  Post.find().exec((err, posts) => {
-      res.render('index', { title: 'My Blog', posts });
+  res.render('layout', { title: 'My Blog', content: "posts/new" });
+});
+
+
+/* GET /posts/:id -> view.ejs */
+router.get('/:id', function(req, res, next) {
+  Post.findById(req.params.id, (err, post) => {
+      res.render('layout', { title: 'My Blog', content: "posts/view", post });
   })
 });
+
+
 
 
 /* GET  /posts/create */
 router.post('/create', function(req, res, next) {
-  Post.find().exec((err, posts) => {
-      res.render('index', { title: 'My Blog', posts });
+    
+  let post = { posted_at: new Date()};
+  Object.assign(post, req.body);
+
+  new Post(post).save((err) =>{
+      
+      res.redirect(301, "/posts/");
   })
 });
 
@@ -36,16 +42,20 @@ router.post('/create', function(req, res, next) {
 
 /* GET  /posts/:id/edit -> edit.ejs */
 router.get('/:id/edit', function(req, res, next) {
-  Post.find().exec((err, posts) => {
-      res.render('index', { title: 'My Blog', posts });
+  Post.findById(req.params.id, (err, post) => {
+      res.render('layout', { title: 'My Blog', content: "posts/edit", post });
   })
 });
 
 
 /* GET  /posts/:id/update */
 router.post('/:id/update', function(req, res, next) {
-  Post.find().exec((err, posts) => {
-      res.render('index', { title: 'My Blog', posts });
+  let post = { posted_at: new Date()};
+  Object.assign(post, req.body);
+
+  new Post(post).save((err) =>{
+      
+      res.redirect(301, "/posts/");
   })
 });
 
@@ -76,33 +86,33 @@ router.post('/:id/delete', function(req, res, next) {
 
 
 
-router.get('/new', function(req, res, next) {
-  Post.find().exec((err, posts) => {
-      res.render('new', { title: 'My Blog', posts });
-  })
-});
-
-
-router.post('/', function(req, res, next) {
-  let post = {posted_at: new Date()};
-  Object.assign(post, req.body);
-    console.log(post);
-    
-  new Post(post).save(err => {
-      res.render('index', { title: 'My Blog'});
-  });
-});
-
-router.get('/:id', function(req, res, next) {
-  let post = undefined;
-  for(let p of posts){
-    if(p.id == req.params.id){
-      post = p;
-      break;
-    }
-  }
-  res.render('index', { title: 'My Blog', post });
-});
+//router.get('/new', function(req, res, next) {
+//  Post.find().exec((err, posts) => {
+//      res.render('new', { title: 'My Blog', posts });
+//  })
+//});
+//
+//
+//router.post('/', function(req, res, next) {
+//  let post = {posted_at: new Date()};
+//  Object.assign(post, req.body);
+//    console.log(post);
+//    
+//  new Post(post).save(err => {
+//      res.render('index', { title: 'My Blog'});
+//  });
+//});
+//
+//router.get('/:id', function(req, res, next) {
+//  let post = undefined;
+//  for(let p of posts){
+//    if(p.id == req.params.id){
+//      post = p;
+//      break;
+//    }
+//  }
+//  res.render('index', { title: 'My Blog', post });
+//});
 
 
 module.exports = router;
