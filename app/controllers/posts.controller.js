dee7e2d9ee563baca7692be3a346c20a366f9990
Abdulmalek Al-Gahmi <aalgahmi@EH313-00-14436.local>
2018-10-09 10:20:00 -0600

    After class of tue of week7

--- app/controllers/posts.controller.js
+++ app/controllers/posts.controller.js
@@ -24,8 +24,6 @@ router.get('/:id', function(req, res, next) {
 });
 
 
-
-
 /* GET  /posts/create */
 router.post('/create', function(req, res, next) {
     
@@ -50,26 +48,32 @@ router.get('/:id/edit', function(req, res, next) {
 
 /* GET  /posts/:id/update */
 router.post('/:id/update', function(req, res, next) {
-  let post = { posted_at: new Date()};
-  Object.assign(post, req.body);
-
-  new Post(post).save((err) =>{
-      
+  Post.findById(req.params.id, (err, post) => {
+    Object.assign(post, req.body);
+    post.save((err) =>{
       res.redirect(301, "/posts/");
-  })
+    });
+  });
 });
 
 
 /* post  /posts/:id/delete */
 router.post('/:id/delete', function(req, res, next) {
-  Post.find().exec((err, posts) => {
-      res.render('index', { title: 'My Blog', posts });
-  })
+  Post.findByIdAndRemove(req.params.id, (err) => {
+    res.redirect(301, "/posts/");
+  });
 });
 
 
 
-
+router.post("/:id/comments/create", function(req, res, next){
+  Post.findById(req.params.id, (err, post) => {
+    post.comments.push(req.body);
+    post.save((err) =>{
+      res.redirect(301, "/posts/" + post.id);
+    });
+  });
+});
 
 
 
--- app/models/schemas.js
+++ app/models/schemas.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+let commentSchema = new Schema ({
+    user: String,
+    body: String,
+    posted_at: Date
+});
+
 let postSchema = new Schema ({
     title: String,
     author: String,
     summary:String,
     posted_at: Date,
-    body: String
+    body: String,
+    comments: [commentSchema]
 })
 
 module.exports.Post = mongoose.model("Post", postSchema);
